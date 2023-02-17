7941cd46f9c266cd5a7aa998642b48b0bc0056a5
DouglasDGA <douglas.dgalbuquerqu@senacsp.edu.br>
2023-02-16 22:54:41 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExercíciosDeFixação4
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = new SqlConnection();
+        SqlCommand comando = new SqlCommand();
+        SqlDataReader dr;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comando.Connection = conn;
+            CarregarLista();
+        }
+        private void CarregarLista()
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            listBox4.Items.Clear();
+
+            conn.Open();
+            comando.CommandText = "select * from Exercicio4";
+            dr = comando.ExecuteReader();
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    listBox1.Items.Add(dr[1].ToString());
+                    listBox2.Items.Add(dr[2].ToString());
+                    listBox3.Items.Add(dr[3].ToString());
+                    listBox4.Items.Add(dr[4].ToString());
+
+                }
+            }
+            conn.Close();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox l = sender as ListBox;
+            if (l.SelectedIndex != -1)
+            {
+                listBox1.SelectedIndex = l.SelectedIndex;
+                listBox2.SelectedIndex = l.SelectedIndex;
+                listBox3.SelectedIndex = l.SelectedIndex;
+                listBox4.SelectedIndex = l.SelectedIndex;
+
+                textBox1.Text = listBox1.SelectedItem.ToString();
+                textBox2.Text = listBox2.SelectedItem.ToString();
+                textBox3.Text = listBox3.SelectedItem.ToString();
+                textBox4.Text = listBox4.SelectedItem.ToString();
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox l = sender as ListBox;
+            if (l.SelectedIndex != -1)
+            {
+                listBox1.SelectedIndex = l.SelectedIndex;
+                listBox2.SelectedIndex = l.SelectedIndex;
+                listBox3.SelectedIndex = l.SelectedIndex;
+                listBox4.SelectedIndex = l.SelectedIndex;
+
+                textBox1.Text = listBox1.SelectedItem.ToString();
+                textBox2.Text = listBox2.SelectedItem.ToString();
+                textBox3.Text = listBox3.SelectedItem.ToString();
+                textBox4.Text = listBox4.SelectedItem.ToString();
+            }
+        }
+
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox l = sender as ListBox;
+            if (l.SelectedIndex != -1)
+            {
+                listBox1.SelectedIndex = l.SelectedIndex;
+                listBox2.SelectedIndex = l.SelectedIndex;
+                listBox3.SelectedIndex = l.SelectedIndex;
+                listBox4.SelectedIndex = l.SelectedIndex;
+
+                textBox1.Text = listBox1.SelectedItem.ToString();
+                textBox2.Text = listBox2.SelectedItem.ToString();
+                textBox3.Text = listBox3.SelectedItem.ToString();
+                textBox4.Text = listBox4.SelectedItem.ToString();
+            }
+        }
+
+        private void listBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox l = sender as ListBox;
+            if (l.SelectedIndex != -1)
+            {
+                listBox1.SelectedIndex = l.SelectedIndex;
+                listBox2.SelectedIndex = l.SelectedIndex;
+                listBox3.SelectedIndex = l.SelectedIndex;
+                listBox4.SelectedIndex = l.SelectedIndex;
+
+                textBox1.Text = listBox1.SelectedItem.ToString();
+                textBox2.Text = listBox2.SelectedItem.ToString();
+                textBox3.Text = listBox3.SelectedItem.ToString();
+                textBox4.Text = listBox4.SelectedItem.ToString();
+            }
+        }
+    }
+}
---
+++ Pessoa.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercíciosDeFixação4
+{
+    class Pessoa
+    {
+        private string funcionario;
+        private string sexo;
+        private string salario;
+        private string media;
+        public string Funcionario
+        {
+            get { return funcionario; }
+        }
+
+        public string Sobrenome
+        {
+            get { return sexo; }
+        }
+
+        public string Salario
+        {
+            get { return salario; }
+            set { salario = value; }
+        }
+
+        public string Media
+        {
+            get { return media; }
+            set { media = value; }
+        }
+
+        public Pessoa(string funcionario, string sexo, string salario, string media)
+        {
+            this.funcionario = funcionario;
+            this.sexo = sexo;
+            this.salario = salario;
+            this.media = media;
+        }
+    }
+}
